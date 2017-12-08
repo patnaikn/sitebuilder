c9adf6d3de73a3b57b91d0cb4a9b4642773706cf
patnaikn <namrata.patnaik@cdk.com>
2017-12-08 17:11:24 +0530

    second commit

--- siteBuilder/src/app/app.module.ts
+++ siteBuilder/src/app/app.module.ts
@@ -2,21 +2,35 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule, Routes} from "@angular/router";
 
 import { AppComponent } from './app.component';
 import { ConsumerComponent } from './consumer/consumer.component';
 import { EditorComponent } from './editor/editor.component';
+import { PropertiesComponent } from './properties/properties.component';
+import { StylesComponent } from './styles/styles.component';
+import { AdminComponent } from './admin/admin.component';
+
+const appRoutes : Routes = [
+  {path: 'properties', component: PropertiesComponent },
+  {path: 'styles', component: StylesComponent},
+  {path: 'admin', component: AdminComponent}
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     ConsumerComponent,
-    EditorComponent
+    EditorComponent,
+    PropertiesComponent,
+    StylesComponent,
+    AdminComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
